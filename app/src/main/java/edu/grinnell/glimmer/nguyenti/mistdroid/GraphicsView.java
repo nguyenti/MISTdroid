3694a392e895add019cb3bce023b481705145892
nguyenti <tn.xc.lax@gmail.com>
2015-04-20 22:28:56 -0500

    Clean up code and add some comments

--- app/src/main/java/edu/grinnell/glimmer/nguyenti/mistdroid/GraphicsView.java
+++ app/src/main/java/edu/grinnell/glimmer/nguyenti/mistdroid/GraphicsView.java
@@ -25,35 +25,19 @@ import static android.graphics.Bitmap.Config;
 public class GraphicsView extends View {
 
 
-    private int gridSize = 200;
-    private int strokeWidth = 50;
-
     private Paint paintBg;
-    private Paint paintPoints;
-    private Paint paintwidth;
 
+    int height = 100;
+    int width = 100;
+    Bitmap bitmap = Bitmap.createBitmap(new DisplayMetrics(), width, height, Config.ARGB_8888);
 
     public GraphicsView(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-
         paintBg = new Paint();
-//        paintBg.setColor(Color.BLUE);
-
-        paintPoints = new Paint();
-        paintPoints.setColor(Color.RED);
-
-        paintwidth = new Paint();
-        paintwidth.setStrokeWidth(strokeWidth);
-        paintwidth.setColor(Color.RED);
-
 
     }
 
-    int height = 100;
-    int width = 100;
-    int round = 0;
-    Bitmap bitmap = Bitmap.createBitmap(new DisplayMetrics(), width, height, Config.ARGB_8888);
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
@@ -61,34 +45,40 @@ public class GraphicsView extends View {
         Rect src = new Rect(0,0,width,height);
         Rect dest = new Rect(0,0,getWidth(),getWidth());
 
-//        Bitmap bitmap = makeBitmap(round);
-//        round = (round + 1) % height;
-//        canvas.drawBitmap(bitmap, src, dest, paintBg);
         try {
             updateBitmap();
         } catch (Exception e) {
             e.printStackTrace();
         }
 
-
         canvas.drawBitmap(bitmap, src, dest, paintBg);
 
         invalidate();
     }
 
+    /**
+     * Set the code of the graphic
+     * @param code
+     * @throws Exception
+     */
     public void codeIt(String code) throws Exception {
         TreeNode d = DAG.makeDAG(Parser.parse(code));
 
         GraphicsModel.getInstance().initializeDAG(new DAGEvaluator(d));
         Log.i("TAG_CREATE_DAG", "Dag created");
-//        updateBitmap();
     }
 
+    /**
+     * Set the bitmap of the graphic. This may need to be changed with time functions
+     * @throws Exception
+     */
     private void updateBitmap() throws Exception {
         int size = height * width;
         int colors[] = new int[size];
         Pixel pix[] = new Pixel[size];
         GraphicsModel instance = GraphicsModel.getInstance();
+
+        // go through "image" and set pixel values
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 double x = j / (width * 1.0) * 2.0 - 1.0;
@@ -104,44 +94,10 @@ public class GraphicsView extends View {
                 colors[j + i * height] = pix[j + i * height].gradientToRGB();
             }
         }
-        /**
-        for (int i = 0; i < size; i++) {
-            if (i >= round * width && i < (round + 1) * width)
-                colors[i] = new Pixel(-1).gradientToRGB();
-            else
-                colors[i] = new Pixel(1).gradientToRGB(); // there's a bug here with 0
-         }
-         **/
         bitmap.setPixels(colors, 0, width, 0, 0, width, height);
-//        invalidate();
     }
 
-    private Bitmap makeBitmap(int round) {
-        int size = height * width;
-        int colors[] = new int[size];
-        for (int i = 0; i < size; i++) {
-            if (i >= round * width && i < (round + 1) * width)
-                colors[i] = new Pixel(1).gradientToRGB();
-            else
-                colors[i] = new Pixel(.01).gradientToRGB(); // there's a bug here with 0
-        }
-        Bitmap bitmap = Bitmap.createBitmap(new DisplayMetrics(), colors, width, height, Config.ARGB_8888);
 
-        return bitmap;
-    }
-
-    private void drawGrid(Canvas canvas) {
-        for (int i = 0; i < 10; i+=strokeWidth) {
-            for (int j = 0; j < 10; j+=strokeWidth) {
-//                Pixel instance = GraphicsModel.getInstance().getFieldContent(i, j);
-//                paintPoints.setColor(instance.gradientToRGB());
-
-                canvas.drawPoint(i,j,paintwidth);
-            }
-        }
-
-        invalidate();
-    }
 
     // to keep the width and height as a square
 //    @Override
